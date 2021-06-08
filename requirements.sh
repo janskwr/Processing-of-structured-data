068f0013d0e4184635fa23b25903ce57230b6b38
Janek <62259501+janskwr@users.noreply.github.com>
2021-06-08 02:36:04 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,14 +4,20 @@ read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     sudo apt -y install r-base
 else
-    echo You already have R.
+    echo OK.
 fi
-sudo apt -y install r-base
 sudo su - -c "R -e \"install.packages('stringi', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('data.table', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('XML', repos='http://cran.rstudio.com/')\""
 sudo wget "https://archive.org/download/stackexchange/beer.stackexchange.com.7z"
 sudo wget "https://archive.org/download/stackexchange/gaming.stackexchange.com.7z"
 sudo wget "https://archive.org/download/stackexchange/health.stackexchange.com.7z"
-echo -n "Do you want to install R-S this a good question (y/n)? "
+echo -n "Do you want to install RStudio (y/n)? "
 read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    sudo apt-get install gdebi-core
+    wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.4.1717-amd64.deb
+    sudo gdebi rstudio-server-1.4.1717-amd64.deb
+else
+    echo OK.
+fi
